bbac88ba72988ecb0be15aae1b231d3b059beb20
thliao <l0404th@gmail.com>
2019-08-22 14:21:25 +0800

    update the getSeqofOG

--- ForOrthofinder/api/getSeqofOG_pro.py
+++ ForOrthofinder/api/getSeqofOG_pro.py
@@ -7,6 +7,7 @@
 import sys
 import os
 from os.path import dirname
+
 sys.path.insert(0, dirname(dirname(__file__)))
 import pandas as pd
 from Bio import SeqIO
@@ -34,47 +35,56 @@ def detect_too_lowgroup(num_each_g):
     return [_[0] for _ in too_low]
 
 
-def select_OG(data, rr, rn, total, group_info):
+def select_OG(data, rr, rn,rgf, total, group_info):
+
     if total <= 0:
         raise Exception("Wrong input total number of genomes")
-    if rr is None and rn is None:
-        raise Exception("no input params")
-    if rr:
-        rn = int(total * rr)
+    if rgf is None:
+        if rr is None and rn is None:
+            raise Exception("no input params")
+        if rr:
+            rn = int(total * rr)
+        else:
+            rr = float(rr / total)
+
+        print("require at least cover %s genomes" % rn)
+        if group_info is None:
+            count_series = data.count(1)
+            keep_OG = list(count_series.index[count_series >= rn])
+            selected_genomes = data.loc[keep_OG, :]
+            selected_genomes = list(selected_genomes.loc[:, ~selected_genomes.isna().all(0)].columns)
+            return keep_OG, selected_genomes
+        else:
+            num_each_g = group_info.value_counts()
+            num_each_g = num_each_g.drop(detect_too_lowgroup(num_each_g))
+            # drop too low manually assigned group
+            id2g = group_info.to_dict()
+            remained_num_each_g = (num_each_g * rr).astype(int).to_dict()
+
+            keep_OG = []
+            copy_data = data.copy()
+            copy_data.columns = list(map(lambda x: id2g.get(x, None),
+                                         list(copy_data.columns)))
+            for rid, row in copy_data.iterrows():
+                subset_row = row[~row.isna()]
+                count_subset = Counter(subset_row.index)
+                # print(count_subset)
+                if all([count_subset.get(g, 0) >= v
+                        for g, v in remained_num_each_g.items()]):
+                    keep_OG.append(rid)
+            print("Found required %s OG " % len(keep_OG))
+
+            selected_subset = data.loc[keep_OG, :]
+            selected_subset = selected_subset.loc[:, ~selected_subset.isna().all(0)]
+            return keep_OG, list(selected_subset.columns)
     else:
-        rr = float(rr / total)
-    print("require at least cover %s genomes" % rn)
-    if group_info is None:
-        count_series = data.count(1)
-        keep_OG = list(count_series.index[count_series >= rn])
-        selected_genomes = data.loc[keep_OG,:]
-        selected_genomes = list(selected_genomes.loc[:, ~selected_genomes.isna().all(0)].columns)
-        return keep_OG, selected_genomes
-    else:
-        num_each_g = group_info.value_counts()
-        num_each_g = num_each_g.drop(detect_too_lowgroup(num_each_g))
-        # drop too low manually assigned group
-        id2g = group_info.to_dict()
-        remained_num_each_g = (num_each_g * rr).astype(int).to_dict()
+        selected_genomes = open(rgf).read().split('\n')
+        selected_genomes = [_ for _ in selected_genomes if _ and _ in data.columns]
 
-        keep_OG = []
-        copy_data = data.copy()
-        copy_data.columns = list(map(lambda x: id2g.get(x, None),
-                                     list(copy_data.columns)))
-        for rid, row in copy_data.iterrows():
-            subset_row = row[~row.isna()]
-            count_subset = Counter(subset_row.index)
-            # print(count_subset)
-            if all([count_subset.get(g, 0) >= v
-                    for g, v in remained_num_each_g.items()]):
-                keep_OG.append(rid)
-        print("Found required %s OG " % len(keep_OG))
-
-        selected_subset = data.loc[keep_OG, :]
-        selected_subset = selected_subset.loc[:, ~selected_subset.isna().all(0)]
-        # import pdb;pdb.set_trace()
-        return keep_OG, list(selected_subset.columns)
-
+        keep_OG = data.loc[:,selected_genomes]
+        keep_OG = list(keep_OG.loc[~keep_OG.isna().any(1), ].index)
+        print("current %s selected genomes and %s OG presented at all these genomes" % (len(selected_genomes),len(keep_OG)))
+        return keep_OG,selected_genomes
 
 def get_group_info(group_file,
                    group_column, ):
@@ -99,17 +109,19 @@ def do_mafft(indir, suffix='faa'):
 @click.command()
 @click.option("-i", "infile")
 @click.option("-o", "output_dir", help="the directory of output to")
-@click.option("--only_ortholog", help="the directory of output to", is_flag=True, default=True)
+@click.option("--only_ortholog", help="", is_flag=True, default=True)
 @click.option("-rr", "remained_ratio", help='choose ', default=0.5)
 @click.option("-rn", "remained_num", help='choose ', default=None)
+@click.option("-rgf", "remained_genomes_file", help="selecting single copy OG according a file whihc each line is a genome", default=None)
 @click.option("-g", "group_file", help='choose ', default=None)
 @click.option("-gc", "group_column", default=None)
-@click.option("-doMSA", "doMSA", default=True, is_flag=True)
+@click.option("-doMSA", "doMSA", help='do MSA with mafft or not?', default=False, is_flag=True)
 def main(infile,
          output_dir,
          only_ortholog,
          remained_ratio,
          remained_num,
+         remained_genomes_file,
          group_file,
          group_column,
          doMSA
@@ -120,15 +132,19 @@ def main(infile,
         get_OGs, result_genome = select_OG(data,
                                            rr=remained_ratio,
                                            rn=remained_num,
+                                           rgf=remained_genomes_file,
                                            total=data.shape[1],
-                                           group_info=group_info)
+                                           group_info=group_info,
+                                           )
         os.makedirs(output_dir, exist_ok=True)
         with open(join(output_dir, 'selected_genomes.txt'), 'w') as f1:
             f1.write('\n'.join(result_genome))
+        tqdm.write("Extracting required %s OG" % len(get_OGs))
         get_seq_with_OG(infile,
                         get_OGs,
                         output_dir=output_dir)
         if doMSA:
+            tqdm.write("performing the MSA process")
             do_mafft(output_dir)
     else:
         # keep OG with paralog
