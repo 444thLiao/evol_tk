a84d5e1c8902e61dc16ef0fce1cb7523e33db8db
thliao <l0404th@gmail.com>
2019-09-11 10:25:47 +0800

    upload work

--- grab_Whole_metabolism/api/update_tab_with_ncbi_id.py
+++ grab_Whole_metabolism/api/update_tab_with_ncbi_id.py
@@ -6,11 +6,12 @@ from tqdm import tqdm
 Entrez.email = 'l0404th@gmail.com'
 
 a = pd.read_excel('manually_curated_N_cycle_genes.xlsx')
+a = pd.read_excel('../test2.xlsx')
 for _, row in tqdm(a.iterrows(), total=a.shape[0]):
     aa_seq = row['AA sequence']
-    if not pd.isna(row['AA sequence(seq)']):
+    #if not pd.isna(row['AA sequence(seq)']):
         # got before
-        continue
+        #continue
     if pd.isna(aa_seq):
         aa_seq = row['nucl sequence']
     if len(aa_seq) <= 100:
@@ -18,9 +19,12 @@ for _, row in tqdm(a.iterrows(), total=a.shape[0]):
         record = Entrez.read(Entrez.esearch(term=pid, db='protein'))
         if record.get('IdList', []):
             id = ','.join([str(_) for _ in record.get('IdList', [])])
-            protein_seq = Entrez.efetch(db='protein', id=id, retmode='text', rettype='fasta')
-            protein_seq = SeqIO.read(protein_seq, format='fasta')
-            a.loc[_, 'AA sequence(seq)'] = str(protein_seq.seq)
+            exact_id = Entrez.read(Entrez.esummary(db='protein',id=id))
+            exact_pid = exact_id[0].get('AccessionVersion',pid)
+            a.loc[_, 'AA accession'] = str(exact_pid)
+            #protein_seq = Entrez.efetch(db='protein', id=id, retmode='text', rettype='fasta')
+            #protein_seq = SeqIO.read(protein_seq, format='fasta')
+            #a.loc[_, 'AA sequence(seq)'] = str(protein_seq.seq)
             nuc_record = Entrez.read(Entrez.elink(dbfrom='protein', id=id, db='nuccore'))
             if len(nuc_record) != 0:
                 nuc_record = nuc_record[0]
@@ -28,18 +32,24 @@ for _, row in tqdm(a.iterrows(), total=a.shape[0]):
                 if not _cache:
                     continue
                 nuc_id = nuc_record.get('LinkSetDb', [{}])[0].get('Link', [{}])[0].get('Id')
-                nuc_seq = Entrez.efetch(db='nuccore', id=nuc_id, retmode='text', rettype='fasta')
-                nuc_seq = SeqIO.read(nuc_seq, format='fasta')
-                a.loc[_, 'nucl sequence(seq)'] = str(nuc_seq.seq)
+                exact_id = Entrez.read(Entrez.esummary(db='nuccore', id=nuc_id))
+                exact_nid = exact_id[0].get('AccessionVersion', pid)
+                a.loc[_, 'nucl accession'] = str(exact_nid)
+                # nuc_seq = Entrez.efetch(db='nuccore', id=nuc_id, retmode='text', rettype='fasta')
+                # nuc_seq = SeqIO.read(nuc_seq, format='fasta')
+                # a.loc[_, 'nucl sequence(seq)'] = str(nuc_seq.seq)
             else:
                 continue
         else:
             record = Entrez.read(Entrez.esearch(term=pid, db='nuccore'))
             if record.get('IdList', []):
                 id = ','.join([str(_) for _ in record.get('IdList', [])])
-                nuc_seq = Entrez.efetch(db='nuccore', id=id, retmode='text', rettype='gb')
-                nuc_seq = SeqIO.read(nuc_seq, format='genbank')
-                a.loc[_, 'nucl sequence(seq)'] = str(nuc_seq.seq)
+                exact_id = Entrez.read(Entrez.esummary(db='nuccore', id=id))
+                exact_nid = exact_id[0].get('AccessionVersion', pid)
+                a.loc[_, 'nucl accession'] = str(exact_nid)
+                # nuc_seq = Entrez.efetch(db='nuccore', id=id, retmode='text', rettype='gb')
+                # nuc_seq = SeqIO.read(nuc_seq, format='genbank')
+                # a.loc[_, 'nucl sequence(seq)'] = str(nuc_seq.seq)
                 pro_record = Entrez.read(Entrez.elink(dbfrom='nuccore', id=id, db='protein'))
                 if len(pro_record) != 0:
                     protein_record = pro_record[0]
@@ -47,13 +57,17 @@ for _, row in tqdm(a.iterrows(), total=a.shape[0]):
                     if not _cache:
                         continue
                     protein_id = protein_record.get('LinkSetDb', [{}])[0].get('Link', [{}])[0].get('Id')
-                    protein_seq = Entrez.efetch(db='protein', id=protein_id, retmode='text', rettype='fasta')
-                    protein_seq = SeqIO.read(protein_seq, format='fasta')
-                    a.loc[_, 'AA sequence(seq)'] = str(protein_seq.seq)
+                    exact_id = Entrez.read(Entrez.esummary(db='protein', id=protein_id))
+                    exact_pid = exact_id[0].get('AccessionVersion', pid)
+                    a.loc[_, 'AA accession'] = str(exact_pid)
+                    # protein_seq = Entrez.efetch(db='protein', id=protein_id, retmode='text', rettype='fasta')
+                    # protein_seq = SeqIO.read(protein_seq, format='fasta')
+                    # a.loc[_, 'AA sequence(seq)'] = str(protein_seq.seq)
                 else:
                     continue
             else:
                 continue
+a.to_excel('../test3.xlsx')
     else:
         a.loc[_, 'AA sequence(seq)'] = str(aa_seq)
 
