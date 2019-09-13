24ce4d1a93650f5e5d1dc121a0b01bde46c588f4
lth at luolab <l0404th@gmail.com>
2019-09-13 09:49:02 +0800

    update get_N_metabolism

--- grab_Whole_metabolism/api/get_N_metabolism.py
+++ grab_Whole_metabolism/api/get_N_metabolism.py
@@ -135,7 +135,8 @@ def get_locusDetailedInfo(locus2info):
             else:
                 info_dict = kegg.parse('ENTRY ' + each_str)
             locus = info_dict.get('ENTRY', 'unknown').split(' ')[0]
-            if locus in genes_df.iloc[:,0]:
+            locus = [_ for _ in bin10 if locus in _][0]
+            if locus in genes_df.iloc[:, 0]:
                 continue
             try:
                 (other_paralog_locus,
