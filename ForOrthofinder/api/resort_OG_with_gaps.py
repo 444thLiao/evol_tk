f4d90ff4aa60272004c6ca890b858f28ba0f6972
lth at luolab <l0404th@gmail.com>
2019-09-26 21:03:29 +0800

    fix bugs

--- ForOrthofinder/api/resort_OG_with_gaps.py
+++ ForOrthofinder/api/resort_OG_with_gaps.py
@@ -22,6 +22,45 @@ def order_a_column(Acolumn):
                               index=[locus2OG[locus] for locus in sorted_column])
     return sorted_series
 
+def get_next_locus_idx(genome_ordered_col,used_locus):
+    reorder_col = sorted(genome_ordered_col + [used_locus], key=order_func)
+    # this is a ascending list
+    # add this locus into these OG, and reorder it.
+    # maybe reverse alignment, so it should compare the next and up idx.
+    used_locus_idx = reorder_col.index(used_locus)
+    if used_locus_idx == len(reorder_col) - 1:
+        # the last one, no next, it mean it is the largest one
+        # if right order, up_locus_idx is the last one, this locus should insert into the end
+        # if reverse order, up_locus_idx is the first one, this locus should insert into the front
+        next_locus_idx = -1
+        # right order mean up_locus_idx is 
+    else:
+        next_locus = reorder_col[used_locus_idx + 1]
+        next_locus_idx = genome_ordered_col.index(next_locus)
+        
+    if used_locus_idx == 0:
+        # the first one, no up, it mean it is the smallest one
+        # if right order, next_locus_idx is the first one, this locus should insert into the front
+        # if reverse order, next_locus_idx is the last one, this locus should insert into the end
+        up_locus_idx = -1
+    else:
+        up_locus = reorder_col[used_locus_idx - 1]
+        up_locus_idx = genome_ordered_col.index(up_locus)
+        
+    if (next_locus_idx == -1 and next_locus_idx == 0) or (up_locus_idx == -1 and next_locus_idx==len(genome_ordered_col)):
+        # insert into the end
+        return len(genome_ordered_col)
+    elif (next_locus_idx == -1 and next_locus_idx == len(genome_ordered_col)) or (up_locus_idx == -1 and next_locus_idx==0):
+        # insert into the front
+        return 0
+
+    if next_locus_idx < up_locus_idx:
+        # if original next one smaller than up one, it mean reversed.
+        # so we should insert this locus into the front of up one instead of next one
+        return up_locus_idx
+    else:
+        return next_locus_idx
+
 
 def main(infile, backbone_column_idx=0):
     OG_df = pd.read_csv(infile, sep='\t', index_col=0)
@@ -41,20 +80,15 @@ def main(infile, backbone_column_idx=0):
 
     for gap_OG, row in tqdm(gap_OG_df.iterrows(),
                             total=gap_OG_df.shape[0]):
+
         used_genome, used_locus = [(genome, locus)
                                    for genome, locus in row.items()
                                    if not pd.isna(locus)][0]
-        # get the first not nan one. 
+        # iterative to get the first not nan one to order whole row.
         genome_ordered_col = list(OG_df.reindex(order_OG_without_gap).loc[:, used_genome])
         # order this genome among all order_OG_without_gap OGs. (may nan, but for backbone is all full and order.)
-        reorder_col = sorted(genome_ordered_col + [used_locus], key=order_func)
-        # add this locus into these OG, and reorder it.
-        next_locus = reorder_col[reorder_col.index(used_locus) + 1]
-        # find the next locus
-        if not pd.isna(next_locus):
-            insert_idx = genome_ordered_col.index(next_locus)
-        else:
-            insert_idx = len(order_OG_without_gap)
+        insert_idx = get_next_locus_idx(genome_ordered_col,used_locus)
+        # find the next locus(need to justify the order of this index, not arbitrary +1), and take the index as the insert index.
         order_OG_without_gap.insert(insert_idx, gap_OG)
     final_OG_df = pd.concat([order_OG_df, gap_OG_df]).reindex(order_OG_without_gap)
     return final_OG_df
