3d45cf8b242a99c209554802b5bb2249f2698039
lth at luolab <l0404th@gmail.com>
2019-10-26 10:59:34 +0800

    fix bugs

--- global_search/reannotate_tmp.py
+++ global_search/reannotate_tmp.py
@@ -2,6 +2,7 @@ import pandas as pd
 source_file = '/home-user/thliao/resource/NCBI2habitat.csv'
 import sys
 from os.path import exists,join
+from tqdm import tqdm
 
 if len(sys.argv) >= 2:
     file_list = sys.argv[1:]
@@ -11,19 +12,27 @@ if len(sys.argv) >= 2:
     source_df = source_df.set_index('tmp')
     source_df = source_df.loc[~source_df.index.duplicated(), :]
     
-    for fdir in file_list:
-        f = join(fdir,'full_info.xlsx')
-        full_df = pd.read_excel(f,index_col=0)
-        if exists(source_file):
+    failed_f = []
+    for fdir in tqdm(file_list):
+        try:
+            f = join(fdir,'full_info.xlsx')
+            full_df = pd.read_excel(f,index_col=0)
+            if exists(source_file):
 
-            # bioproject
-            tmp = [';'.join(list(map(str, row))[:2])
-                    for row in full_df.loc[:, ['BioProject', 'BioSample']].values]
+                # bioproject
+                tmp = [';'.join(list(map(str, row))[:2])
+                        for row in full_df.loc[:, ['BioProject', 'BioSample']].values]
 
-            _d1 = source_df.reindex(tmp)
-            for idx, (_, v) in enumerate(full_df.iterrows()):
-                if not pd.isna(_d1.iloc[idx, 2]) and pd.isna(v['habitat']):
-                    # print(_,_d1.iloc[idx,2])
-                    full_df.loc[_, 'habitat'] = _d1.iloc[idx, 2]
-        full_df.to_excel(f.replace('.xlsx','_new.xlsx'),
-                            index=1, index_label='protein accession')
+                _d1 = source_df.reindex(tmp)
+                for idx, (_, v) in enumerate(full_df.iterrows()):
+                    if not pd.isna(_d1.iloc[idx, 2]) and pd.isna(v['habitat']):
+                        # print(_,_d1.iloc[idx,2])
+                        full_df.loc[_, 'habitat'] = _d1.iloc[idx, 2]
+            full_df.to_excel(f.replace('.xlsx','_new.xlsx'),
+                                index=1, index_label='protein accession')
+        except:
+            failed_f.append(fdir)
+    if failed_f:
+        print(len(failed_f),' failed')
+    else:
+        print('all successfull')
