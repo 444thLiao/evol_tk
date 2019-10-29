0f448dc12c781ab9c60622bc03e6585755acaa18
lth at luolab <l0404th@gmail.com>
2019-10-29 14:08:11 +0800

    implement a pid2tax py

--- bin/ncbi_convert/pid2GI.py
+++ bin/ncbi_convert/pid2GI.py
@@ -32,17 +32,21 @@ def pid2GI(id_list):
 
 def main(infile, ofile, force=False):
     order_id_list, id2annotate = parse_id(infile)
-    if isinstance(next(id2annotate.values()), dict):
+    id2gi = {}
+    if isinstance(id2annotate[order_id_list[0]], dict):
         # it is a dict, so it contains other infomation or implemented GI. it may be passed over.
-        if 'GI' in next(id2annotate.values()):
+        if 'GI' in id2annotate[order_id_list[0]]:
             print("provided file already contains `GI` column(doesn't check the validation/completeness). Giving `force` param to overwrite/implement it. ")
             if not force:
-                return
+                id2gi = {k:id2annotate[k]['GI'] for k in order_id_list}
+            
         # todo: re-implemented original infomation into `ofile` from `infile`
     else:
         # no header, just a list of IDs
         pass
-    id2gi = pid2GI(order_id_list)
+    if not id2gi:
+        id2gi = pid2GI(order_id_list)
+    
     if not exists(dirname(ofile)):
         os.makedirs(dirname(ofile))
 
