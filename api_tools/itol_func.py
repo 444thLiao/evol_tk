2180ef144d53231a68a79aa6641ba7b147f0778c
lth at luolab <l0404th@gmail.com>
2019-11-04 18:12:49 +0800

    upload work

--- api_tools/itol_func.py
+++ api_tools/itol_func.py
@@ -65,12 +65,15 @@ def annotate_outgroup(ID2infos, info2style,):
         annotate_text += '\t'.join(row) + '\n'
     return template_text + annotate_text
 
-def to_binary_shape(ID2info,info2style, info_name='dataset',one_col=False):
+def to_binary_shape(ID2info,info2style, info_name='dataset',manual_v=[]):
     # id2info, could be {ID:list/set}
     # info2color: could be {gene1: {shape:square,color:blabla},}
     # None will use default.
     template_text = open(dataset_binary_template).read()
-    all_v = list(sorted(set([_ for v in ID2info.values() for _ in v if _])))
+    if not manual_v:
+        all_v = list(sorted(set([_ for v in ID2info.values() for _ in v if _])))
+    else:
+        all_v = manual_v
     annotate_text = []
     for ID,vset in ID2info.items():
         row = '\t'.join([ID] + ['1' if _ in vset else '0' for _ in all_v])
@@ -237,7 +240,7 @@ def to_node_symbol(in_tree,dataset_name='bootstrap'):
             color = '#000000'
         elif int(s_v) >= 85 and int(s_v) < 95:
             color = '#777777'
-        elif int(s_v) >= 85 and int(s_v) < 95:
+        elif int(s_v) >= 65 and int(s_v) < 85:
             color = '#eeeeee'
         else:
             color = ''
--- global_search/reannotate_genes_contains_info.py
+++ global_search/reannotate_genes_contains_info.py
@@ -23,42 +23,62 @@ def reformat(s):
             return s
         else:
             return s.rpartition('_')[0]
+under_nodes = {'nxrA':['I49_S100', 'I137_S88', 'I88_S99', 'I71_S100'],
+              'hao':['I49_S100','I32_S100'],
+              'amoA':['I43_S100','I30_S100',
+                      #'I15_S76'
+                      ],
+              'amoB':['I44_S89','I71_S100',
+                      #'I14_S100','I16_S100'
+                      ],
+              'amoC':['I12_S100','I57_S59'],
+              }
 
 all_files = ['nr_retrieve_amoB/with_genome_Bacteria_drop_NC10_intact.faa_aln.dir/iqtree.treefile',
              'nr_retrieve_amoC/with_genome_Bacteria_drop_NC10_intact.faa_aln.dir/iqtree.treefile',
              'with_genome_amoA/with_genome_Bacteria_drop_NC10_intact.faa_aln.dir/iqtree.treefile',
              'nr_retrieve_hao/with_genome_Bacteria_intact.faa_aln.dir/iqtree.treefile',
+             'nr_retrieve_nxrA/with_genome_Bacteria_drop_NC10_intact_lt_600.faa_aln.dir/iqtree.treefile'
              #'nr_retrieve_nxrA/with_genome_Bacteria_intact.faa_aln.dir/cluster_95_aln.dir/iqtree.treefile'
              ]
-# check_call('python3 ~/script/evolution_relative/global_search/reannotate_tree.py '+ ' '.join(all_files),shell=1)
+check_call('python3 ~/script/evolution_relative/global_search/reannotate_tree.py '+ ' '.join(all_files),shell=1)
 from collections import defaultdict
 g_genomes = defaultdict(list)
 genome2id = []
+reformat_id2ori = {}
+manuall_class_ids = []
 for f in all_files:
-    
     g = f.split('/')[0].split('_')[-1]
     tree = glob(join(f,'*.sorted.newick'))[0]
     t = Tree(tree,format=1)
-    all_ids = t.get_leaf_names()
-    all_ids = [reformat(_).strip() for _ in all_ids]
+    all_ids = [reformat(_).strip() for _ in t.get_leaf_names()]
+    if under_nodes.get(g,[]):
+        _ns = [_ 
+               for _ in t.iter_descendants() 
+               if _.name in under_nodes.get(g,[])]
+        for _n in _ns:
+            manuall_class_ids += list(_n.get_leaf_names())
+        manuall_class_ids = [reformat(_).strip() for _ in manuall_class_ids]
+    reformat_id2ori.update(dict(zip(all_ids,
+                                    [_ for _ in t.get_leaf_names()])))
     pro2genome = pd.read_csv(join(f,'info_dir','pro2genome.tab'),
                              sep='\t',index_col=0)
     pro2genome = pro2genome.fillna('None')
-    genomes_assembly = []
-    for _ in all_ids:
-        if _ in pro2genome.index:
-            _cache = pro2genome.loc[_,'assembly_ID']
+    for gene in all_ids:
+        if gene in pro2genome.index:
+            _cache = pro2genome.loc[gene,'assembly_ID']
             if isinstance(_cache,str) and _cache !='None':
-                genomes_assembly.append(_cache)
-                genome2id.append((_cache,_))
+                genome2id.append((_cache,gene))
+                if gene in manuall_class_ids:
+                    g_genomes[g].append(_cache)
             elif str(_cache) == 'None':
-                print(_,g)
+                print(gene,g)
             else:
-                genomes_assembly+=list(_cache)
-                genome2id+=[(_g,_) for _g in _cache]
+                if gene in manuall_class_ids:
+                    g_genomes[g] += list(_cache)
+                genome2id += [(_g,gene) for _g in list(_cache)]
         else:
-            print(_,g)
-    g_genomes[g] = genomes_assembly
+            print(gene,g)
 
 genome2genes = defaultdict(list)
 for g,genomes in g_genomes.items():
@@ -68,8 +88,11 @@ for g,genomes in g_genomes.items():
 
 id2genes = {}
 for genome,genes in genome2genes.items():
-    ids = [id for g,id in genome2id if g == genome]
-    id2genes.update({id:list(sorted(set(genes))) for id in ids})
+    ids = [id 
+           for g,id in genome2id 
+           if g == genome]
+    id2genes.update({id:list(set(genes)) 
+                     for id in ids})
 
 info2style = {'amoA':{'color':'#ff0000',
                       'info':'amoA'},
@@ -79,17 +102,21 @@ info2style = {'amoA':{'color':'#ff0000',
                       'info':'amoC'},
               'hao':{'color':'#b68100',
                       'info':'hao'},
+              'nxrA':{'color':'#4b85c1',
+                      'info':'nxrA'},
               }
-odir = ''
+#odir = ''
 for f in all_files:
+    g = f.split('/')[0].split('_')[-1]
     tree = glob(join(f,'*.sorted.newick'))[0]
     t = Tree(tree,format=1)
-    all_ids = t.get_leaf_names()
-    all_ids = [reformat(_).strip() for _ in all_ids]
-    this_id2genes = {_:id2genes.get(_,[]) 
+    all_ids = [reformat(_).strip() for _ in t.get_leaf_names()]
+        
+    this_id2genes = {reformat_id2ori[_]:id2genes.get(_,[]) 
                      for _ in all_ids 
                      if _ in id2genes}
     all_text = to_binary_shape(this_id2genes,
-                    info2style,info_name='genes set')
+                    info2style,
+                    info_name='genes set',manual_v=['amoA','amoB','amoC','hao','nxrA'])
     with open(join(f,'gene_set.txt'),'w') as f1:
         f1.write(all_text)
