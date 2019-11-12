77818190b3655c30c6148d4a8573d172e8234333
thliao <l0404th@gmail.com>
2019-11-12 23:18:00 +0800

    fix

--- ForOrthofinder/api/concat_aln.py
+++ ForOrthofinder/api/concat_aln.py
@@ -50,9 +50,9 @@ def generate_phy_file(outfile, record_pos_info):
         for name, start, end, aln_record in record_pos_info:
             num_seq = len(aln_record)
             length_this_aln = aln_record.get_alignment_length()
-            f1.write(f'{num_seq} {length_this_aln}\n')
+            f1.write(f'{num_seq}        {length_this_aln}\n')
             for _ in range(num_seq):
-                f1.write(f"{aln_record[_,:].id} {str(aln_record[_,:].seq)}\n")
+                f1.write(f"{aln_record[_,:].id}        {str(aln_record[_,:].seq)}\n")
             
 @click.command(help="For concating each aln, if it has some missing part of specific genome, it will use gap(-) to fill it")
 @click.option("-i", "indir", help="The input directory which contains all separate aln files")
---
+++ dating_workflow/step_script/s1_codeml.py
@@ -0,0 +1,2 @@
+
+from ete3 import Tree
