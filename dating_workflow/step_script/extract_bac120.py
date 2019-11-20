9057d6fa9f3eb501342f85a89a368b1597a39e4a
thliao <l0404th@gmail.com>
2019-11-20 22:56:46 +0800

    update script

--- dating_workflow/step_script/extract_bac120.py
+++ dating_workflow/step_script/extract_bac120.py
@@ -1,5 +1,5 @@
 from glob import glob
-from subprocess import check_call
+from subprocess import check_call, check_output
 import os
 from tqdm import tqdm
 from os.path import *
@@ -7,58 +7,89 @@ from collections import defaultdict
 from Bio import SeqIO
 import multiprocessing as mp
 
-pfam_db = '/home-db/pub/protein_db/Pfam/Pfam.v32.0/Pfam-A.hmm'
-tigfam_db_dir = '/home-user/thliao/data/protein_db/TIGRFAM/'
-
+pfam_db = '/home-user/thliao/data/protein_db/bac120/Pfam.v32.sub6.hmm'
+tigfam_db = '/home-user/thliao/data/protein_db/bac120/TIGRFAMv14_sub114.hmm'
 
 __file__ = '/home-user/thliao/script/evolution_relative/dating_workflow/step_script/extrat_bac120.py'
-bac120_list = join(dirname(__file__), 'cog.list')
-id_list = [row.split('\t')[0] for row in open(bac120_list) if _]
+bac120_list = join(dirname(__file__), 'bac120.tsv')
+id_list = [row.split('\t')[0] for row in open(bac120_list) if row]
 id_list = id_list[1:]
 pfam_ids = [_ for _ in id_list if _.startswith('PF0')]
 tigfam_ids = [_ for _ in id_list if _.startswith('TIGR')]
+
+
 # ABOVE is the default setting for luolab server.
 
 
 def run(cmd):
     check_call(cmd,
-               shell=True)
+               shell=True,
+               stdout=open('/dev/null', 'w'),
+               )
 
 
-def annotate_gene(raw_protein, out_cog_dir,type='pfam'):
+def annotate_bac120(protein_files, odir, db_id='pfam'):
     params = []
-    for f in tqdm(glob(raw_protein)):
-        gname = f.split('/')[-1].replace('.faa', '')
-        if type == 'pfam':
-            ofile = f'{out_cog_dir}/{gname}.out'
-            cmd = f"hmmscan --tblout {ofile} --acc --noali --notextw --cpu 40 {pfam_db}  {f}"
-        else:
-            ofile = f'{out_cog_dir}/{gname}.{type}.out'
-            assert exists(f"{tigfam_db_dir}/{type}.HMM")
-            cmd = f"hmmscan --tblout {ofile} --acc --noali --notextw --cpu 40 {tigfam_db_dir}/{type}.HMM  {f}"
-        if not os.path.exists(ofile):
+    if not exists(odir):
+        os.makedirs(odir)
+    for pfile in protein_files:
+        gname = basename(pfile).replace('.faa', '')
+        if db_id == 'pfam':
+            ofile = f'{odir}/PFAM/{gname}.out'
+            cmd = f"/usr/local/bin/hmmscan --tblout {ofile} --acc --noali --notextw --cpu 40 {pfam_db} {pfile}"
+        elif db_id == 'tigrfam':
+            ofile = f'{odir}/TIGRFAM/{gname}.out'
+            cmd = f"/usr/local/bin/hmmscan --tblout {ofile} --acc --noali --notextw --cpu 40 {tigfam_db} {pfile}"
+        # else:
+        #     ofile = f'{odir}/{db_id}/{gname}.out'
+        #     assert exists(f"{tigfam_db_dir}/{db_id}.HMM")
+        #     cmd = f"/usr/local/bin/hmmscan --tblout {ofile} --acc --noali --notextw --cpu 40 {tigfam_db_dir}/{db_id}.HMM {pfile}"
+        if not exists(ofile):
+            if not exists(dirname(ofile)):
+                os.makedirs(dirname(ofile))
             params.append(cmd)
             # check_call(cmd, shell=1)
+    # print(params)
     with mp.Pool(processes=5) as tp:
-        list(tqdm(tp.imap(run, params), total=len(params)))
+        for _ in tp.imap(run, tqdm(params)):
+            pass
 
 
-def extra_gene(cog_out_dir):
-    # cog out dir
-    genome2cdd = defaultdict(lambda: defaultdict(list))
-    for f in tqdm(glob(join(cog_out_dir, '*.out'))):
-        genome_name = f.split('/')[-1].replace('.out', '')
-        for row in open(f, 'r'):
-            locus = row.split('\t')[0]
-            if row.split('\t')[1] in cdd_num:
-                genome2cdd[genome_name][row.split('\t')[1]].append(locus)
-    return genome2cdd
+def parse_grep_result(ofile,filter_evalue):
+    # for grep against hmm scan
+    gid2locus = defaultdict(list)
+    for row in open(ofile, 'r'):
+        if row.startswith('#'):
+            continue
+        r = row.split(' ')
+        r = [_ for _ in r if _]
+
+        gene_id = r[1]
+        locus_tag = r[2]
+        evalue = r[4]
+        if float(evalue) <= filter_evalue :
+            gid2locus[gene_id].append(locus_tag)
+    return gid2locus
+
+
+def extra_genes(odir,evalue=1e-10):
+    genome2gene_id = defaultdict(dict)
+    odir_pfam = join(odir, 'PFAM')
+    odir_tigrfam = join(odir, 'TIGRFAM')
+    outfiles = glob(join(odir_pfam, '*.out')) + glob(join(odir_tigrfam, '*.out'))
+    tqdm.write('start to read/parse output files')
+    for outfile in tqdm(outfiles):
+        gname = basename(outfile).replace('.out', '')
+        gid2locus = parse_grep_result(outfile,evalue)
+        genome2gene_id[gname].update(gid2locus)
+    return genome2gene_id
 
 
 # extract protein
-def write_cog_multiple(outdir, genome2cdd, raw_proteins):
-    pdir = dirname(expanduser(raw_proteins))
-    for genome_name, pdict in tqdm(genome2cdd.items()):
+def write_genes_multiple(outdir, genome2gene_id, protein_files):
+    pdir = dirname(expanduser(protein_files[0]))
+    genome2gene_seq = {}
+    for genome_name, pdict in tqdm(genome2gene_id.items()):
         pfiles = glob(f'{pdir}/{genome_name}.faa')
         if pfiles:
             pfile = pfiles[0]
@@ -68,24 +99,25 @@ def write_cog_multiple(outdir, genome2cdd, raw_proteins):
                         for _ in v
                         if _ in _cache]
                     for k, v in pdict.items()}
-            genome2cdd[genome_name] = pset
+            genome2gene_seq[genome_name] = pset
         else:
             continue
+    all_ids = set([_ for v in genome2gene_seq.values() for _ in v.keys()])
     # concat/output proteins
     if not exists(outdir):
         os.makedirs(outdir)
-    for each_cdd in tqdm(cdd_num):
-        cdd_records = []
-        for gname, p_d in genome2cdd.items():
-            get_records = p_d.get(each_cdd, [])
+    for gene_id in tqdm(all_ids):
+        gene_records = []
+        for gname, p_d in genome2gene_seq.items():
+            get_records = p_d.get(gene_id, [])
             if len(get_records) >= 1:
                 # record = get_records
                 for record in get_records:
                     record.name = gname
-                cdd_records += get_records
-        with open(join(outdir, f"{each_cdd.replace('CDD:', '')}.faa"), 'w') as f1:
-            SeqIO.write(cdd_records, f1, format='fasta-2line')
-
+                gene_records += get_records
+        with open(join(outdir, f"{gene_id}.faa"), 'w') as f1:
+            SeqIO.write(gene_records, f1, format='fasta-2line')
+    return genome2gene_seq
 
 def perform_iqtree(outdir):
     script = expanduser('~/bin/batch_run/batch_mafft.py')
@@ -95,13 +127,17 @@ def perform_iqtree(outdir):
     run(f"python3 {script} -i {outdir} -o {outdir} -ns newick -use fasttree")
 
 
-def stats_cog(genome2cdd, outdir):
-    cog_multi = defaultdict(list)
-    for g, _d in genome2cdd.items():
-        for cog, v in _d.items():
-            if len(v) >= 2:
-                cog_multi[cog].append(g)
-
+def stats_cog(genome2genes):
+    gene_multi = defaultdict(int)
+    for genome, pdict in genome2genes.items():
+        for gene, seqs in pdict.items():
+            if len(seqs) >= 2:
+                gene_multi[gene] += 1
+    gene_Ubiquity = defaultdict(int)
+    for genome, pdict in genome2genes.items():
+        for gene, seqs in pdict.items():
+            gene_Ubiquity[gene] += 1
+    return gene_multi,gene_Ubiquity
 
 if __name__ == "__main__":
     import sys
@@ -114,9 +150,15 @@ if __name__ == "__main__":
         outdir = sys.argv[3]
     else:
         raw_proteins = expanduser('~/data/nitrification_for/dating_for/raw_genome_proteins/*.faa')
-        out_cog_dir = expanduser('~/data/nitrification_for/dating_for/target_genes')
-        outdir = expanduser('~/data/nitrification_for/dating_for/conserved_protein')
-    annotate_cog(raw_proteins, out_cog_dir)
-    genome2cdd = extra_cog(out_cog_dir)
-    write_cog_multiple(outdir, genome2cdd)
+        out_cog_dir = expanduser('~/data/nitrification_for/dating_for/bac120_annoate')
+        outdir = expanduser('~/data/nitrification_for/dating_for/bac120_annoate/seq')
+
+        protein_files = glob(raw_proteins)
+    # for tigfam_id in tigfam_ids:
+    annotate_bac120(protein_files, out_cog_dir, db_id='tigrfam')
+    annotate_bac120(protein_files, out_cog_dir, db_id='pfam')
+
+    genome2genes = extra_genes(out_cog_dir)
+    genome2gene_seq = write_genes_multiple(outdir, genome2genes,protein_files)
+    stats_cog(genome2genes)
     perform_iqtree(outdir)
