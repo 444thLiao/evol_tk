e0e990722d8eab73e71ea1c1ce3282d36d6ed7ab
lth at luolab <l0404th@gmail.com>
2019-12-04 15:31:47 +0800

    add get_gene script

--- dating_workflow/step_script/__init__.py
+++ dating_workflow/step_script/__init__.py
@@ -1,5 +1,6 @@
 
 from collections import defaultdict
+from os.path import *
 def _get_tophit(gid2locus,top_hit):
                 
     if top_hit:
@@ -49,3 +50,30 @@ def _parse_hmmscan(ofile,filter_evalue=None,top_hit = False):
             gid2locus[gene_id].append((locus_tag, evalue))
     gid2locus = _get_tophit(gid2locus,top_hit=top_hit)
     return gid2locus
+
+def process_path(path):
+    if '~' in path:
+        path = expanduser('path')
+    if not '/' in path:
+        path = './' + path
+    path = abspath(path)
+    return path
+
+# two function for dating workflow(formatting the name of assembly genomes)
+def convert_genome_ID(genome_ID):
+    # for GCA_900078535.2
+    # it will return
+    if isinstance(genome_ID,str) and genome_ID.startswith('GC'):
+        return genome_ID.split('_')[-1].replace('.', 'v')
+    else:
+        return genome_ID
+
+def convert_genome_ID_rev(locus_ID):
+    # for 900078535v2
+    # it will return
+    if isinstance(locus_ID,str):
+        if '_' in locus_ID:
+            locus_ID = locus_ID.partition('_')[0]
+        return 'GCA_' + locus_ID.replace('v', '.')
+    else:
+        return locus_ID
---
+++ dating_workflow/step_script/get_gene_list_with_percentage.py
@@ -0,0 +1,52 @@
+import click
+from glob import glob
+from os.path import *
+from dating_workflow.step_script import convert_genome_ID,convert_genome_ID_rev,process_path
+from Bio import SeqIO
+import os
+
+def main(indir,suffix,num_genomes):
+    all_genes = glob(join(indir,f'*.{suffix}'))
+    all_genes_name = [basename(_).replace(f".{suffix}","")
+                  for _ in all_genes]
+    
+    genes_need = []
+    for f,name in zip(all_genes,all_genes_name):
+        records = SeqIO.parse(f,format='fasta')
+        num_records = len(list(records))
+        if num_records>=num_genomes:
+            genes_need.append(name)
+    return genes_need
+    
+@click.command()
+@click.option('-i','indir')
+@click.option("-o", "ofile", default=None, help="path of outfile.")
+@click.option('-s','suffix',default='aln')
+@click.option("-gl", "genome_list", default=None, help="it will read 'selected_genomes.txt', please prepare the file, or indicate the alternative name or path.")
+@click.option('-num_p','num_percentage',default=None,help="1-100")
+@click.option('-num','num_genomes',default=None)
+def cli(indir,ofile,suffix,num_percentage,num_genomes,genome_list):
+    if genome_list is None:
+        genome_list = join(indir, 'selected_genomes.txt')
+    with open(genome_list, 'r') as f1:
+        gids = f1.read().split('\n')
+    gids = [convert_genome_ID(_) for _ in gids]
+    
+    if num_genomes is None and num_percentage is None:
+        num_percentage = 100
+        num_genomes = len(gids)
+    elif num_genomes is None:
+        num_genomes = len(gids)*num_percentage/100
+    else:
+        num_genomes = int(num_genomes)
+    genes = main(indir,suffix,num_genomes)
+    
+    ofile = process_path(ofile)
+    if not exists(dirname(ofile)):
+        os.makedirs(dirname(ofile))
+    print(f"found {len(genes)} meet requirement.")
+    with open(ofile,'w') as f1:
+        f1.write('\n'.join(genes))
+    
+if __name__ == "__main__":
+    cli()
