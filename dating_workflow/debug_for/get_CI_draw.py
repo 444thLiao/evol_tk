1231491dba6897a24f5b0ade18df621f4c6e5005
lth at luolab <l0404th@gmail.com>
2019-12-07 09:07:56 +0800

    update script

--- dating_workflow/debug_for/get_CI_draw.py
+++ dating_workflow/debug_for/get_CI_draw.py
@@ -12,7 +12,7 @@ warnings.filterwarnings('ignore')
 infile = './03_mcmctree.out'
 ofile = './evaluate.png'
 
-def draw_picture(infile,ofile,return_fig=False):
+def get_vals(infile):
     f = open(infile).read().split('\n')
     head = 'Posterior mean (95% Equal-tail CI) (95% HPD CI) HPD-CI-width'
     if head not in f:
@@ -35,31 +35,51 @@ def draw_picture(infile,ofile,return_fig=False):
             posterior_mean,equal_tailed_95p_down,equal_tailed_95p_up = map(format_v,vals[1:4])
             CI_collect.append((equal_tailed_95p_up-equal_tailed_95p_down))
             mean_collect.append(posterior_mean)
-    
     df = pd.DataFrame()
-
     df.loc[:,'CI_width'] = CI_collect
     df.loc[:,'Posterior mean time (100 Ma)'] = mean_collect
+    
+    return df
 
-
+def get_r2(text):
+    r_squre_text = text['hovertemplate'].split('<br>')[2]
+    prefix,v = r_squre_text.split('=')
+    mode_text = text['hovertemplate'].split('<br>')[4]
+    mode = mode_text.split('=')[-1]
+    
+    return f"{prefix} of {mode}={v}"
+    
+def draw_picture(infile,infile2,ofile,return_fig=False):
+    df1 = get_vals(infile)
+    df2 = get_vals(infile2)
+    
+    df1.loc[:,'mode'] = 'posterior'
+    df2.loc[:,'mode'] = 'prior'
+    df = pd.concat([df1,df2],axis=0)
+    
     fig = px.scatter(df, 
                     x='Posterior mean time (100 Ma)',
                     y="CI_width",
+                    color="mode",
                     trendline="ols")
-    r_squre_text = fig.data[1]['hovertemplate'].split('<br>')[2]
+    
+    r_squre_text = [get_r2(_)
+                    for _ in [fig.data[1],fig.data[3]] ]
+    
     fig.update_layout(
         showlegend=False,
         annotations=[
             go.layout.Annotation(
                 x=0,
-                y=max(CI_collect),
-                text=r_squre_text,
+                y=max(list(df.loc[:,'CI_width'])),
+                text='<br>'.join(r_squre_text),
                 showarrow=False,
                 font=dict(size=20)
             )
         ]
     )
-    r_squre = float(r_squre_text.split('=')[-1])
+    
+    r_squre = float([_.split('=')[-1] for _ in r_squre_text if 'posterior' in _][0])
     if return_fig:
         return fig
     else:
@@ -70,28 +90,27 @@ def draw_picture(infile,ofile,return_fig=False):
 
 collect_r2 = {}
 for infile in glob('./locus_each/*/mcmc_for/03_mcmctree.out'):
+    infile2 = infile.replace("mcmc_for/03_mcmctree.out","prior/nodata_mcmctree.out")
     gene_name = infile.split('/')[2].split('_')[-1]
     ofile = f'./pack_result/test_r2/one_gene{gene_name}.png'
     if not exists(dirname(ofile)):
         os.makedirs(dirname(ofile))
-    r2 = draw_picture(infile,ofile,return_fig=False)
+    r2 = draw_picture(infile,infile2,ofile,return_fig=False)
     collect_r2[gene_name] = r2
 
 fs = glob('./*/mcmc_for/03_mcmctree.out')
 for infile in fs:
+    infile2 = infile.replace("mcmc_for/03_mcmctree.out","prior/nodata_mcmctree.out")
     gene_name = infile.split('/')[1]
     ofile = f'./pack_result/test_r2/{gene_name}.png'
     if not exists(dirname(ofile)):
         os.makedirs(dirname(ofile))
-    r2 = draw_picture(infile,ofile,return_fig=False)
+    r2 = draw_picture(infile,infile2,ofile,return_fig=False)
     collect_r2[gene_name] = r2
     
+    
 infile = '/home-user/thliao/data/nitrification_for/dating_for/rough_dating/01_mcmctree_out/out'
+infile2 = '/home-user/thliao/data/nitrification_for/dating_for/rough_dating/nodata_out/no_data.out'
 ofile = './pack_result/test_r2/rough_dating.png'
-r2 = draw_picture(infile,ofile,return_fig=False)
-collect_r2['rough dating'] = r2
-
-infile = '/home-user/thliao/data/nitrification_for/dating_for/rough_dating/01_mcmctree_out/out'
-ofile = './pack_result/test_r2/rough_dating.png'
-r2 = draw_picture(infile,ofile,return_fig=False)
+r2 = draw_picture(infile,infile2,ofile,return_fig=False)
 collect_r2['rough dating'] = r2
--- dating_workflow/figtree2itol.py
+++ dating_workflow/figtree2itol.py
@@ -5,6 +5,8 @@ import os
 from os.path import *
 from ete3 import Tree
 import re
+import click
+from dating_workflow.step_script import process_path
 #   Tree with NHX style metadata:
 
 #    (A:0.1,(B:0.2,(C:0.2,D:0.3):0.4[&&NHX:conf=0.01:name=NODE1]):0.5);
@@ -27,89 +29,122 @@ def sub_for(m):
 indir = '/home-user/thliao/template_txt/'
 dataset_symbol_template = join(indir,'dataset_symbols_template.txt')
 
-root_with = '''GCA_000011385.1
-GCA_000013205.1
-GCA_000317065.1
-GCA_000332175.1
-GCA_000332215.1
-GCA_000011345.1
-GCA_000022045.1
-GCA_000018105.1'''.split('\n')
-ori_tre = "/home-user/thliao/data/nitrification_for/dating_for/bac120_annoate/concat/243g/iqtree.treefile"
-tree2 = Tree(ori_tre)
-tree2.set_outgroup(tree2.get_common_ancestor(root_with))
+
+
+def main(intree_ori,mcmc_out_tree,output_dating_result_tree,itol_annotate,root_with):
+    # root_with = '''GCA_000011385.1
+    # GCA_000013205.1
+    # GCA_000317065.1
+    # GCA_000332175.1
+    # GCA_000332215.1
+    # GCA_000011345.1
+    # GCA_000022045.1
+    # GCA_000018105.1'''.split('\n')
+    # ori_tre = "/home-user/thliao/data/nitrification_for/dating_for/bac120_annoate/concat/243g/iqtree.treefile"
+    tree2 = Tree(intree_ori)
+    tree2.set_outgroup(tree2.get_common_ancestor(root_with))
 
 
 
-mcmc_out_tre = './concat_into1/concat_1/mcmc_for/FigTree.tre'
-a = open(mcmc_out_tre)
-for row in a:
-    if row.strip().startswith('UTREE 1 ='):
-        t = row.split('UTREE 1 =')[1].strip('\n')
-        t = re.sub('\[&95%HPD=.*?\]',sub_for,t)
-        t = t.replace(' ','')
-        tree = Tree(t,format=1)
-        
-count = 0 
-for n in tree.traverse():
-    if not n.is_leaf():
-        dates = n.name
-        n.name = 'I%s' % count
-        n.add_features(ages=dates,)
-        all_leafs = n.get_leaf_names()
-        nin2 = tree2.get_common_ancestor(all_leafs)
-        n.add_features(support=nin2.support)
-        count +=1
-# tree.features.remove('support')
-text = tree.write(format=3)
-text = text.replace(')1:','):')
-with open('./pack_result/concat_into1.nexus','w') as f1:
-    f1.write(text)
-    
-    
-raw_text = []
-for n in tree.traverse():
-    if not n.is_leaf():
-        raw_text.append("\t".join([n.name,
-                                   n.ages,
-                                   '1',
-                                   	'#FF0000',
-                                    'bold',
-                                    '1',
-                                    '0'
-                               ]))
-template = open('/home-user/thliao/template_txt/dataset_text_template.txt').read()
-with open('../itol_txt/itol_ages.txt','w') as f1:
-    f1.write(template+'\n'+'\n'.join(raw_text))
-
-
-rows = []
-template_text = open(dataset_symbol_template).read()
-for n in tree2.traverse():
-    if not n.is_leaf():
-        size = '5'
-        shape = '2'
-        filled = '1'
-        s_v = n.support
-        childrens = n.get_leaf_names()
-        nid = tree.get_common_ancestor(childrens)
-        
-        if int(s_v) >= 95:
-            color = '#000000'
-        elif int(s_v) >= 85 and int(s_v) < 95:
-            color = '#777777'
-        elif int(s_v) >= 65 and int(s_v) < 85:
-            color = '#eeeeee'
-        else:
-            color = '#FFFFFF'
+    # mcmc_out_tre = './concat_into1/concat_1/mcmc_for/FigTree.tre'
+    mcmc_out_tree_text = open(mcmc_out_tree)
+    for row in mcmc_out_tree_text:
+        if row.strip().startswith('UTREE 1 ='):
+            t = row.split('UTREE 1 =')[1].strip('\n')
+            t = re.sub('\[&95%HPD=.*?\]',sub_for,t)
+            t = t.replace(' ','')
+            tree = Tree(t,format=1)
             
-        if color:
-            row = '\t'.join([nid.name,shape,size,color,filled,'0',''])
-            rows.append(row)
+    count = 0 
+    for n in tree.traverse():
+        if not n.is_leaf():
+            dates = n.name
+            n.name = 'I%s' % count
+            n.add_features(ages=dates,)
+            all_leafs = n.get_leaf_names()
+            nin2 = tree2.get_common_ancestor(all_leafs)
+            n.add_features(support=nin2.support)
+            count +=1
+    # tree.features.remove('support')
+    text = tree.write(format=3)
+    text = text.replace(')1:','):')
+    with open(output_dating_result_tree,'w') as f1:
+        f1.write(text)
+        
+        
+    raw_text = []
+    for n in tree.traverse():
+        if not n.is_leaf():
+            raw_text.append("\t".join([n.name,
+                                    n.ages,
+                                    '1',
+                                        '#FF0000',
+                                        'bold',
+                                        '1',
+                                        '0'
+                                ]))
+    template = open('/home-user/thliao/template_txt/dataset_text_template.txt').read()
+    with open(join(itol_annotate,'dating_tree__ages.txt'),'w') as f1:
+        f1.write(template+'\n'+'\n'.join(raw_text))
 
-    annotate_text = '\n'.join(rows)
-    template_text = template_text.format(dataset_label='bootstrap',
-                                         legend_text='',
-                                         maximum_size=10)
-with open('./itol_txt/bootstrap.txt','w') as f1:
-    f1.write(template_text + annotate_text)
+
+    rows = []
+    template_text = open(dataset_symbol_template).read()
+    for n in tree2.traverse():
+        if not n.is_leaf():
+            size = '5'
+            shape = '2'
+            filled = '1'
+            s_v = n.support
+            childrens = n.get_leaf_names()
+            nid = tree.get_common_ancestor(childrens)
+            
+            if int(s_v) >= 95:
+                color = '#000000'
+            elif int(s_v) >= 85 and int(s_v) < 95:
+                color = '#777777'
+            elif int(s_v) >= 65 and int(s_v) < 85:
+                color = '#eeeeee'
+            else:
+                color = '#FFFFFF'
+                
+            if color:
+                row = '\t'.join([nid.name,shape,size,color,filled,'0',''])
+                rows.append(row)
+
+        annotate_text = '\n'.join(rows)
+        template_text = template_text.format(dataset_label='bootstrap',
+                                            legend_text='',
+                                            maximum_size=10)
+    with open(join(itol_annotate,'dating_tree_bootstrap.txt'),'w') as f1:
+        f1.write(template_text + annotate_text)
+    
+    
+
+@click.command()
+@click.option('-i','intree_ori')
+@click.option('-i2','mcmc_out_tree')
+@click.option('-o','output_dating_result_tree')
+@click.option('-od','itol_annotate',default='./itol_txt')
+@click.option('-r','root_with',help='multiple genes could use comma to separate them. LCA would be searched and taken as outgroup')
+def cli(intree_ori,mcmc_out_tree,output_dating_result_tree,itol_annotate,root_with,force):
+    output_dating_result_tree = process_path(output_dating_result_tree)
+    itol_annotate = process_path(itol_annotate)
+    if ',' in root_with:
+        root_with = [_.strip() for _ in root_with.split(',')]
+    else:
+        root_with = [root_with.strip()]
+    
+    if not os.path.exists(dirname(output_dating_result_tree)):
+        os.makedirs(output_dating_result_tree)
+    main(intree_ori,mcmc_out_tree,output_dating_result_tree,itol_annotate,root_with)
+    
+
+# intree_ori = ''
+# root_with = ''
+# mcmc_out_tree = ''
+# output_dating_result_tree = ''
+# itol_annotate = './itol_txt'
+
+if __name__ == "__main__":
+    cli()
