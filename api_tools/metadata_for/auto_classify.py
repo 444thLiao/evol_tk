8ee2fcc9b45d48c10b5a91cea3eb26baf6ffb4ac
lth at luolab <l0404th@gmail.com>
2019-12-18 13:40:43 +0800

    add metadata

--- api_tools/metadata_for/auto_classify.py
+++ api_tools/metadata_for/auto_classify.py
@@ -1,62 +1,50 @@
 import pandas as pd
 from tqdm import tqdm
-from os.path import join
+from os.path import join,expanduser
 from api_tools.itol_func import *
 
+fpath = expanduser("~/script/evolution_relative/api_tools/metadata_for/keyword.csv")
+keyword_mapping =  dict([row.strip('\n').splti('\t') for row in open(fpath).readlines() if row])
+
 def _classificated(ori_df):
-    kw1='classification(auto)'
+    # kw1='classification(auto)'
     kw2='habitat(auto)'
-    ori_df.loc[:,kw1] = ''
+    kw3='collected keyword(auto)'
+    ori_df.loc[:,kw3] = ''
     ori_df.loc[:,kw2] = ''
 
     for _,row in tqdm(ori_df.iterrows(),total=ori_df.shape[0]):
-        row_text = ';'.join(map(str,row.values)).lower()
-        if 'metageno' in row_text:
-            ori_df.loc[_,kw1] = 'MAGs'
-        if 'single cell' in row_text:
-            ori_df.loc[_,kw1] = 'SAGs'
-        if 'soil' in row_text or 'terrestrial' in row_text or 'wetland' in row_text:
-            ori_df.loc[_,kw2] = 'terrestrial'
-        if 'sea' in row_text or 'marine' in row_text or 'ocean' in row_text:
-            ori_df.loc[_,kw2] = 'marine'
-        if 'groundwater' in row_text:
-            ori_df.loc[_,kw2] = 'ground water'
-        if 'bioreactor' in row_text:
-            ori_df.loc[_,kw2] = 'bioreactor'
-        if ('bioreactor' in row_text and 'sludge' in row_text) or 'wastewater' in row_text or 'activated sludge' in row_text:
-            ori_df.loc[_,kw2] = 'waste water'
-        if 'freshwater' in row_text:
-            ori_df.loc[_,kw2] = 'freshwater'
-        if 'soda lake' in row_text:
-            ori_df.loc[_,kw2] = 'alkaline aquatic'
-        if 'subsurface' in row_text:
-            ori_df.loc[_,kw2] = 'subsurface'
-        if 'biofilter' in row_text or 'sand filter' in row_text:
-            ori_df.loc[_,kw2] = 'artificial system'
+        row_text = ' ; '.join(map(str,row.values)).lower()
+        _cache3 = set()
+        _cache2 = set()
+        for kw in keyword_mapping:
+            if kw in row_text:
+                _cache3.add(kw)
+                _cache2.add(keyword_mapping[kw])
+            ori_df.loc[_,'kw2'] = ';'.join(sorted(_cache2))
+            ori_df.loc[_,'kw3'] = ';'.join(sorted(_cache3))
+        # if 'metageno' in row_text:
+        #     ori_df.loc[_,kw1] = 'MAGs'
+        # if 'single cell' in row_text:
+        #     ori_df.loc[_,kw1] = 'SAGs'
         if 'Whole genome' in row_text or 'type strain' in row_text:
             ori_df.loc[_,kw1] = 'isolate'
 
         # if not pd.isna(row['attribute:host']) and str(row['attribute:host']) != 'not applicable':
         #     ori_df.loc[_,'habitat'] = 'host associated'
-        if 'source' in row.index:
-            if 'uncultured' in row['source'] or 'unidentified' in row['source']:
-                ori_df.loc[_,kw2] = 'amplicons'
-        list_c = list(ori_df.columns)
-        list_c = list_c[:-2]
-        list_c.insert(12,kw2)
-        list_c.insert(12,kw1)
-        ori_df = ori_df.reindex(columns=list_c)
+        
     return ori_df
-    # ori_df.to_csv('./nr_retrieve_hao/test.csv',index=1)
     
 def diff_marine_non_marine(ori_df):
     ori_df = ori_df.copy()
     kw2='habitat(auto,diff marine/non-marine)'
     ori_df.loc[:,kw2] = ''
 
-    for _,row in tqdm(ori_df.iterrows(),total=ori_df.shape[0]):
+    for _,row in tqdm(ori_df.iterrows(),
+                      total=ori_df.shape[0]):
+        row = row[[_ for _ in row.index if not '(auto)' in _]]
         row_text = ';'.join(map(str,row.values)).lower()
-        if 'sea' in row_text or ('marine' in row_text and 'non-marine' not in row_text) or 'ocean' in row_text :
+        if ('marine' in row_text and 'non-marine' not in row_text) or 'ocean' in row_text :
             ori_df.loc[_,kw2] = 'marine'
         elif 'soda lake' in row_text:
             ori_df.loc[_,kw2] = 'marine'
@@ -64,12 +52,12 @@ def diff_marine_non_marine(ori_df):
             ori_df.loc[_,kw2] = 'marine'
         elif ('bioreactor' in row_text and 'sludge' in row_text) or 'wastewater' in row_text or 'activated sludge' in row_text:
             ori_df.loc[_,kw2] = 'marine'
-        elif pd.isna(ori_df.loc[_,['habitat(auto)','habitat']]).all():
+        elif pd.isna(ori_df.loc[_,['habitat(auto)']]).all():
             ori_df.loc[_,kw2] = 'unknown'
-        elif str(ori_df.loc[_,'habitat'])=='marine':
-            ori_df.loc[_,kw2] = 'marine'
-        elif str(ori_df.loc[_,'habitat'])=='non-marine':
-            ori_df.loc[_,kw2] = 'non-marine'
+        # elif str(ori_df.loc[_,'habitat'])=='marine':
+        #     ori_df.loc[_,kw2] = 'marine'
+        # elif str(ori_df.loc[_,'habitat'])=='non-marine':
+        #     ori_df.loc[_,kw2] = 'non-marine'
         else:
             ori_df.loc[_,kw2] = 'non-marine'
         list_c = list(ori_df.columns)
--- bin/habitat_auto.py
+++ bin/habitat_auto.py
@@ -1,11 +1,13 @@
-from api_tools.metadata_for.auto_classify import *
+from api_tools.metadata_for.auto_classify import _classificated,diff_marine_non_marine
 from api_tools.IO_for.read import read_table
 import click
 
 def main(infile):
     df = read_table(infile,sep='\t',index_col=None)
     pre_df = _classificated(df)
-
+    general_df = diff_marine_non_marine(pre_df)
+    general_df.to_excel('./auto_habitat.xlsx',index=0)
+    
 @click.command()
 @click.option("-i")
 def cli():
