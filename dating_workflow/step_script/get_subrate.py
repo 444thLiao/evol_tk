9cce24ab00962c7224426f652f07bd3895813207
lth at luolab <l0404th@gmail.com>
2020-01-02 18:19:24 +0800

    update work

--- dating_workflow/step_script/get_subrate.py
+++ dating_workflow/step_script/get_subrate.py
@@ -12,10 +12,7 @@ template_dir = join(dirname(dirname(__file__)), 'ctl_template')
 codeml_ctl = join(template_dir, 'codeml.ctl')
 aaRatefile = join(template_dir, 'lg.dat')
 
-paml_bin = "/home-user/thliao/software/paml4.9j/bin"
-
-
-
+paml_bin = "/home-user/software/paml/v4.9/paml4.9e/bin/codeml"
 
 def modify(file, **kwargs):
     text = open(file).read()
@@ -49,6 +46,43 @@ def run(args):
         with open(log, 'w') as f1:
             f1.write(t)
             
-odir = './SubRate'
-phy_files = '195g_concat.phy'
-gids = ''
+odir = './dating_for/phy_files/195g_SubRate'
+aln_dir = './cog25_single/195g_aln/'
+genome_id = './dating_for_195g.list'
+in_treefile = abspath('./dating_for/phy_files/195g_SubRate/195g_point.fasttree.newick')
+if not exists(odir):
+    os.makedirs(odir)
+    
+cmds = []
+for f in glob(join(aln_dir,'*.trimal')):
+    ofile = join(odir,basename(f).replace('.trimal','.phy'))
+    if not exists(ofile):
+        run(f'python3 ~/script/evolution_relative/dating_workflow/step_script/aln2phy.py -i {f} -o {ofile} -gl {genome_id} -rm_I -no_fill')
+
+
+    if not exists(odir):
+        os.makedirs(odir)
+        
+    seqfile_b = abspath(ofile)
+    treefile_b = in_treefile
+    outfile = f"./{basename(f).replace('.trimal','.phy')}.out"
+    seqtype = 2
+    clock = 1
+    aaRatefile = aaRatefile
+    param = {'seqfile': seqfile_b,
+             'treefile': treefile_b,
+             'seqtype': seqtype,
+             'outfile': outfile,
+             'clock': clock,
+             'aaRatefile': aaRatefile}
+    text = modify(codeml_ctl,**param)
+    ctl_f = join(odir,basename(f).replace('.trimal','_codelml.ctl'))
+    with open(ctl_f,'w') as f1:
+        f1.write(text)
+        
+    cmds.append(f"cd {dirname(ctl_f)}; {paml_bin} {ctl_f} ")
+
+import multiprocessing as mp
+with mp.Pool(processes=30) as tp:
+    r = list(tqdm(tp.imap(run, cmds), total=len(cmds)))
+                
