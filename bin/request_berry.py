368311744ee289726e8d3d9f48ee763552f78a74
thliao <l0404th@gmail.com>
2020-01-17 17:07:25 +0800

    update get_berry

--- bin/request_berry.py
+++ bin/request_berry.py
@@ -7,6 +7,7 @@ import urllib
 import warnings
 from os.path import *
 
+import click
 import requests
 from tqdm import tqdm
 
@@ -26,51 +27,66 @@ def download_file(url, local_filename):
     # return local_filename
 
 
-warnings.filterwarnings('ignore')
-post_data = {'func': 'get_list',
-             'ssid': '02D9JBk',
-             'fid': '02D9JBk',
-             'path': '/191101_X515_0555_AH2MVCCCX2',
-             'sort': 'natural',
-             'dir': 'ASC'}
-r = requests.post('https://ss.berrygenomics.com/share.cgi',
-                  post_data,
-                  verify=False)
-data = json.load(io.StringIO(r.text))
-total = data['total']
-start = 0
-done_count = 0
-while done_count <= total:
-    for f_dict in tqdm(data['datas']):
-        filename = f_dict['filename']
-        _p = dict(ssid=post_data['ssid'],
-                  fid=post_data['fid'],
-                  path=post_data['path'],
-                  filename=filename,
-                  openfolder='forcedownload',
-                  ep='',
-                  start=start)
-        url_r = "https://ss.berrygenomics.com/share.cgi?" + urllib.parse.urlencode(_p)
-        if exists(filename):
-            if int(f_dict['filesize']) == getsize(filename):
-                continue
-        tqdm.write('downloading %s' % filename)
-        download_file(url_r, filename)
-        done_count += 1
-    start += 50
-    post_data.update({'start': start})
+def main(ssid, path, dry_run):
+    warnings.filterwarnings('ignore')
+    post_data = {'func': 'get_list',
+                 'ssid': ssid,
+                 'fid': ssid,
+                 'path': f'/{path}',
+                 'sort': 'natural',
+                 'dir': 'ASC'}
     r = requests.post('https://ss.berrygenomics.com/share.cgi',
                       post_data,
                       verify=False)
+    d = []
     data = json.load(io.StringIO(r.text))
+    total = data['total']
+    start = 0
+    done_count = 0
+    while done_count <= total:
+        for f_dict in data['datas']:
+            filename = f_dict['filename']
+            _p = dict(ssid=post_data['ssid'],
+                      fid=post_data['fid'],
+                      path=post_data['path'],
+                      filename=filename,
+                      openfolder='forcedownload',
+                      ep='',
+                      start=start)
+            url_r = "https://ss.berrygenomics.com/share.cgi?" + urllib.parse.urlencode(_p)
+
+            if exists(filename):
+                if int(f_dict['filesize']) == getsize(filename):
+                    done_count += 1
+                    continue
+            d.append((url_r, filename))
+            done_count += 1
+        start += 50
+        post_data.update({'start': start})
+        r = requests.post('https://ss.berrygenomics.com/share.cgi',
+                          post_data,
+                          verify=False)
+        data = json.load(io.StringIO(r.text))
+    # start downloading
+    for url_r, filename in tqdm(d):
+        if dry_run:
+            continue
+        download_file(url_r, filename)
+        tqdm.write('downloading %s' % filename)
+    tqdm.write("finishing all......")
+
+
+@click.command()
+@click.argument("ssid")
+@click.argument("dirname")
+@click.option("-d", "--dry_run", "dry_run", default=True, is_flag=True)
+def cli(ssid, dirname, dry_run):
+    main(ssid, dirname, dry_run)
+
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 3:
-        raise Exception("UNKNOWN parameters... first is url, second is local_filename")
-    download_file(sys.argv[1],
-                  sys.argv[2])
+    cli()
 
 # url = "https://ss.berrygenomics.com/share.cgi?ssid=02D9JBk&fid=02D9JBk&path=%2F&filename=191101_X515_0555_AH2MVCCCX2&openfolder=forcedownload&ep="
 # download_file(url,'download_2019-11-13_15-00-51.zip')
+# python request_berry.py ssid filename --dry_run
