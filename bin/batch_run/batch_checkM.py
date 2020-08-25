48c5ffe171ac7956ad5212a21e7abb07e7f3b0bb
thliao <l0404th@gmail.com>
2020-08-25 09:57:43 +0800

    update batch_checkm

--- bin/batch_run/batch_checkM.py
+++ bin/batch_run/batch_checkM.py
@@ -29,27 +29,33 @@ def unit_run(infile, indir, tax, odir, extra_option):
                stdout=open('/dev/null', 'w'))
 
 
-def main(indir, odir, tax, use_fa, num_parellel, suffix='', force=False,genome_list=None):
+def main(indir, odir, tax, use_fa, num_parellel, suffix='', force=False, genome_list=None):
     suffix = suffix.strip('.')
     if not exists(odir):
         os.makedirs(odir)
     if suffix:
         suffix = '.' + suffix
     file_list = glob(join(indir, f'*{suffix}'))
-    tqdm.write("start to process %s file with '%s' as suffix" % (len(file_list), suffix))
-    params = []
+
     subset_names = []
     if genome_list is not None:
         subset_names = [_ for _ in open(genome_list).read().split('\n') if _]
 
-    for in_file in file_list:
+    # suffix here already have . as prefix.
+    if subset_names:
+        file_list = [_
+                     for _ in file_list
+                     if basename(_).replace(suffix, '') in subset_names]
+    else:
+        pass
+    tqdm.write("start to process %s file with '%s' as suffix" % (len(file_list),
+                                                                 suffix))
+    params = []
 
-        gname = basename(in_file).replace(f'{suffix}',
-                                                  '')
+    for in_file in file_list:
+        gname = basename(in_file).replace(suffix, '')
         new_odir = join(odir,
                         gname)
-        if subset_names and gname not in subset_names:
-            continue
         if not exists(new_odir) or force:
             params.append((basename(in_file),
                            indir,
@@ -60,17 +66,17 @@ def main(indir, odir, tax, use_fa, num_parellel, suffix='', force=False,genome_l
         r = list(tqdm(tp.imap(run, params), total=len(params)))
 
 
-@click.command()
+@click.command(help="batch run pipelines 'taxonomy_wf' of checkm.")
 @click.option('-i', 'indir', help='input directory')
 @click.option('-o', 'odir', help='output directory')
-@click.option('-s', 'suffix', help='suffix of used files',default='')
+@click.option('-s', 'suffix', help='suffix of used files', default='')
 @click.option('-np', 'num_parellel', default=10)
-@click.option('-t', 'tax', default='domain Bacteria')
+@click.option('-t', 'tax', default='domain Bacteria', help='designated taxonomic level you want to use.')
 @click.option('-f', 'force', help='overwrite?', default=False, required=False, is_flag=True)
 @click.option('-use_fa', 'use_fa', help='use nucleotide sequence or not. default using annotated proteins?', default=False, required=False, is_flag=True)
 @click.option("-gl", "genome_list", default=None,
               help="It could be None. If you provided, you could use it to subset the used sequences by its name.")
-def cli(indir, odir, tax, suffix, force, num_parellel, use_fa,genome_list):
+def cli(indir, odir, tax, suffix, force, num_parellel, use_fa, genome_list):
     main(indir=indir,
          odir=odir,
          tax=tax,
