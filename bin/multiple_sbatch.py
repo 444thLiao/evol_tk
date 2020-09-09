8573c2039917408a8fc5ab8f1cd8ede2aa1dd5db
thliao <l0404th@gmail.com>
2020-09-09 10:13:58 +0800

    sbatch bin

--- bin/multiple_sbatch.py
+++ bin/multiple_sbatch.py
@@ -1,22 +1,37 @@
-import sys
+#!/usr/bin/env python
+
 import os
+import sys
+def mkdir_p(dir):
+    '''make a directory (dir) if it doesn't exist'''
+    if not os.path.exists(dir):
+        print(dir)
+        os.mkdir(dir)
 
-import time
-def get_tasks(f):
-    lines = open(f).read().split('\n')
-    lines = [_ for _ in lines if _]
-    os.system('mkdir -p ./tmp/')
+if __name__ == "__main__":
+    job_directory = f"{os.getcwd()}/.job" 
+    # Make top level directories
+    mkdir_p(job_directory)
 
-    used_files = []
-    for cmd in lines:
-        curr_time = str(int(time.time()))
-        sh_file = f'./tmp/{curr_time}.sh'
-        with open(sh_file,'w') as f1:
-            f1.write("#!/bin/zsh\n")
-            f1.write(cmd)
-        os.system(f'sbatch {sh_file}')
-        used_files.append(sh_file)
-        # os.system(f'rm {sh_file}')
+    cmd_files = sys.argv[1]
+    cmds=[_ for _ in open(cmd_files).read().split('\n')]
 
-if __name__ == '__main__':
-    argv = sys.argv[1]
+    count_ = 0
+    for cmd in cmds:
+        workdir = cmd.split(';')[0].strip().split(' ')[-1]
+        cmd = cmd.split(';')[-1]
+        job_file = os.path.join(job_directory,f"job_lth{count_}.job" )
+
+
+        with open(job_file,'w') as fh:
+            fh.writelines("#!/home-user/thliao/anaconda3/bin/zsh\n")
+            fh.writelines(f"#SBATCH --job-name=job_lth{count_}.job\n")
+            # fh.writelines(f"#SBATCH --output=.out/job_lth{count_}.out\n")
+            # fh.writelines(f"#SBATCH --error=.out/job_lth{count_}.err\n")
+            fh.writelines(f"#SBATCH --workdir={workdir}\n")
+            
+            fh.writelines(cmd)
+
+        os.system("sbatch %s" %job_file)
+        count_ += 1
+    
