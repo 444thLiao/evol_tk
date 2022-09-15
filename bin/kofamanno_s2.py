d899c8b8a0bb7c4052f4049721d34f1bbdd3402c
thliao <l0404th@gmail.com>
2022-09-15 15:20:40 +0800

    new binary file

---
+++ bin/kofamanno_s2.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+import os
+from tqdm import tqdm
+from os.path import *
+import click
+
+_t = '/mnt/maple/thliao/data/NCBI/modified_data/annotations/tab.header'
+header = open(_t).read().strip()+'\n'
+
+kofamscan_exe = '/home-user/thliao/software/kofamscan/exec_annotation'
+ko_list = '/mnt/home-backup/thliao/kofam/20190810/ko_list'
+
+def correct_reanno(ori_tab,odir,ori_faa=None,tmp_dir='/mnt/ivy/thliao/tmp/',existsed_kos=None,remove=True):
+    gid = ori_tab.split('/')[-1].rsplit('.',1)[0]
+    if ori_faa is None:
+        ori_faa = f"/mnt/maple/thliao/data/NCBI/modified_data/direct_protein_files/{gid}.faa"
+    if not exists(ori_tab):
+        raise IOError(f'{ori_tab} is not existsed')
+    all_kos = [([_ for _ in row.split(' ') if _][2],row)
+                for row in open(ori_tab).read().strip().split('\n')
+                if not row.startswith('#')]
+    ko2rows = defaultdict(list)
+    for ko,row in all_kos:
+        if existsed_kos is not None:
+            if ko not in existsed_kos:
+                continue
+        ko2rows[ko].append(row)
+    tmp_dir = tmp_dir+f'/{gid}'
+    if not exists(f"{tmp_dir}/tabular"):
+        os.system(f"mkdir -p {tmp_dir}/tabular")
+    else:
+        os.system(f"rm {tmp_dir}/tabular/*")
+    query_faa = realpath(tmp_dir +'/query.faa')
+    cmd = f"ln -sf `realpath {ori_faa}` {query_faa} "
+    os.system(cmd)
+    for k,rows in tqdm(ko2rows.items(),total=len(ko2rows)):
+        of = tmp_dir+f'/tabular/{k}'
+        with open(of,'w') as f1:
+            f1.write('\n'.join(header+rows))
+    cmd = f"{kofamscan_exe} -r -k  --tmp-dir {tmp_dir} -r -o {odir}/{gid}.kofamout -f mapper-one-line --no-report-unannotated {query_faa} "
+    if remove:
+        cmds += f" ; rm -rf {tmp_dir}"
+    return cmd
+
+
+@click.command()
+@click.option('-i', 'input_table')
+@click.option('-o', 'output_dir')
+@click.option('-tmp', 'tmp_dir',default='/mnt/ivy/thliao/tmp/')
+@click.option('-faa', 'faa',default=None)
+@click.option('-k', 'keep', help='keep the tmp dir or not',default=False, required=False, is_flag=True)
+@click.option('-dry_run', 'dry_run', help='run or not',default=False, required=False, is_flag=True)
+def main(input_table,output_dir,tmp_dir,keep,dry_run,faa):
+    cmd = correct_reanno(input_table,output_dir,
+                         ori_faa=faa,tmp_dir=tmp_dir,
+                         existsed_kos=None,remove=keep)
+    if dry_run:
+        with open('./cmds','a') as f1:
+            f1.write(cmd+'\n')
+    else:
+        os.system(cmd)
+        
+if __name__ == '__main__':
+    main()
